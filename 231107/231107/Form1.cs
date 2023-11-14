a8ab56cbed6b02f9a5248c51b6f3fec39c21a3d9
seh22 <141599911+seh22@users.noreply.github.com>
2023-11-14 13:24:34 +0900

    Add files via upload

---
+++ 231107/231107/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _231107
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            int sum = 0;
+
+            
+        }
+
+        void Test(int[] arr)
+        {
+            for(int i = 0; i< arr.Length; i++)
+            {
+                arr[i] = 1;
+            }
+        }
+        
+        
+
+    }
+}
